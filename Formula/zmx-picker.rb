327791011fccfc4fe9398b70b2d158fc2eeb839a
Aaron Bull Schaefer <aaron@elasticdog.com>
2026-07-17 12:49:56 -0700

    zmx-picker 0.1.0 (new formula)

---
+++ Formula/zmx-picker.rb
@@ -0,0 +1,25 @@
+class ZmxPicker < Formula
+  desc "Fuzzy finder for zmx sessions and repositories"
+  homepage "https://github.com/EarthmanMuons/zmx-picker"
+  url "https://github.com/EarthmanMuons/zmx-picker/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "17acc097750e5ffd85c1c514bd82283f703443567e70aa08bfc9d880c8566017"
+  license "0BSD"
+
+  depends_on "fzf"
+  depends_on "neurosnap/tap/zmx"
+
+  def install
+    bin.install "zp"
+  end
+
+  def caveats
+    <<~EOS
+      Repos are listed from roots given as arguments or $ZP_ROOT.
+      Installing fd (brew install fd) speeds up repository scans.
+    EOS
+  end
+
+  test do
+    assert_match "zp #{version}", shell_output("#{bin}/zp --version")
+  end
+end
